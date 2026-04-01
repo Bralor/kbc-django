1186a990816fe91432fa7272df5b0fe5985d8a78
Matouš Holinka <matous.holinka@dnai.ai>
2026-04-01 21:28:30 +0200

    feat(mysite): update the project after 3rd lesson

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('<int:id>/', views.blog_detail, name='blog_detail'),
+    path('<int:id>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('create/', views.blog_create, name='blog_create'),
+    path('search/', views.blog_search, name='blog_search'),
+    path('list-create/', views.blog_list_create, name='blog_list_create'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,25 +1,76 @@
-from django.http import JsonResponse, HttpRequest
+import json
+
+from django.http import JsonResponse, HttpRequest, Http404, HttpResponse
+from django.shortcuts import render
 from http import HTTPStatus
-from blog.models import Blog
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from django.views import View
+from django.shortcuts import get_object_or_404
+
+from .models import Blog
 
 
 def blog_list(request: HttpRequest):
-    blog_ids = [blog.id for blog in Blog.objects.all()]
-    return JsonResponse(blog_ids, safe=False)
+    blogs = [blog for blog in Blog.objects.all()]
+    return render(request, 'blog/blog_list_static_example.html', {'blogs': blogs})
 
 
-def blog_detail(request: HttpRequest, id: int):
-    try:
-        result = Blog.objects.get(id=id)
+class BlogDetailView(View):
+    def get(self, request: HttpRequest, id: int) -> JsonResponse:
+        blog = get_object_or_404(Blog, id=id)
+
+        return JsonResponse({
+            'id': blog.id,
+            'title': blog.title,
+            'author': blog.author,
+            'published_date': blog.published_date.isoformat(),
+        })
+
+
+@csrf_exempt  # Only for demo/curl testing - in production use CSRF tokens
+def blog_create(request):
+    # Demo-only example: keep it intentionally minimal.
+    if request.method != 'POST':
+        return JsonResponse({'tip': 'Send POST with title, author, published_date'})
+
+    data = json.loads(request.body)
+    blog = Blog.objects.create(
+        title=data['title'],
+        author=data['author'],
+        published_date=data['published_date'],
+    )
+    return JsonResponse({'id': blog.id, 'title': blog.title}, status=201)
+
+
+def blog_search(request: HttpRequest) -> JsonResponse:    
+    query = request.GET.get('q', '')          # 'django' or empty string
+    sort = request.GET.get('sort', 'title')   # 'title' (default)
+    blogs = Blog.objects.all()
     
-    except Blog.DoesNotExist:
-        return JsonResponse(
-            {'error': 'Blog not found'},
-            status=HTTPStatus.NOT_FOUND,
+    if query:
+        blogs = blogs.filter(title__icontains=query)
+    
+    if sort in ('title', 'author', 'published_date'):
+        blogs = blogs.order_by(sort)
+    
+    return JsonResponse(list(blogs.values()), safe=False)
+
+
+@csrf_exempt
+@require_http_methods(["GET", "POST"])
+def blog_list_create(request: HttpRequest) -> JsonResponse:
+    if request.method == 'GET':
+        blogs = list(
+            Blog.objects.order_by('-id').values('id', 'title', 'author', 'published_date')
         )
-    return JsonResponse({
-        'id': result.id,
-        'title': result.title,
-        'author': result.author,
-        'published_date': result.published_date.isoformat()
-    })
+        return JsonResponse({'count': len(blogs), 'results': blogs})
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        blog = Blog.objects.create(
+            title=data['title'],
+            author=data['author'],
+            published_date=data['published_date'],
+        )
+        return JsonResponse({'id': blog.id, 'title': blog.title}, status=201)
